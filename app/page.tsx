e66fd99e0ccbf3fba3205b096434c9a3ac3d1c46
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-20 17:21:41 +0530

    added navbar and routes for their elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center" >website under construction⌛</h1>
+      {/* <h1 className="text-center" >website under construction⌛</h1>
+       */}
+       {/* <Header/> */}
     </div>
   );
 }

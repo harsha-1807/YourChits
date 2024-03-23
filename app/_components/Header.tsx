cf84dea782aa7e7b4920a186fd344b3aa9171e27
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-23 11:23:04 +0530

    signin ,signout added with user profile

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,27 +1,51 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { UserButton, auth } from "@clerk/nextjs";
 
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-function Header() {
+async function Header() {
+  const { userId } = auth();
+
+  // console.log(user);
+
   return (
-<header className="bg-[#07193a]">
-  <div className="mx-auto flex h-16 max-w-screen-2xl items-center gap-8 px-2 sm:px-6 lg:px-10">
-    <Image src="/chitlogo1.svg" alt="logo" width={70} height={80}  />
-    
-    <div className="flex flex-1 items-center justify-end md:justify-between ">
-      <nav aria-label="Global" className="hidden md:block">
-        <ul className="flex items-center gap-6 text-sm">
-          <li>
-            <Link className="text-white transition hover:text-gray-500/75" href="/Plans"> Plans </Link>
-          </li>
-          <li>
-            <Link className="text-white transition hover:text-gray-500/75" href="/About">About</Link>
-          </li>
-           <li>
-            <Link className="text-white transition hover:text-gray-500/75" href="/Contact"> Contact</Link>
-          </li>
-        {/*
+    <header className="bg-[#07193a]">
+      <div className="mx-auto flex h-16 max-w-screen-2xl items-center gap-8 px-2 sm:px-6 lg:px-10">
+        <Link href="/">
+          <Image src="/chitlogo1.svg" alt="logo" width={70} height={80} />
+        </Link>
+
+        <div className="flex flex-1 items-center justify-end md:justify-between ">
+          <nav aria-label="Global" className="hidden md:block">
+            <ul className="flex items-center gap-6 text-sm">
+              <li>
+                <Link
+                  className="text-white transition hover:text-gray-500/75"
+                  href="/Plans"
+                >
+                  {" "}
+                  Plans{" "}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white transition hover:text-gray-500/75"
+                  href="/About"
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-white transition hover:text-gray-500/75"
+                  href="/Contact"
+                >
+                  {" "}
+                  Contact
+                </Link>
+              </li>
+              {/*
           <li>
             <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Services </a>
           </li>
@@ -33,28 +57,34 @@ function Header() {
           <li>
             <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Blog </a>
           </li> */}
+            </ul>
+          </nav>
 
-        </ul>
-      </nav>
+          <div className="flex items-center gap-4">
+            {userId ? (
+              <div className="flex gap-4 items-center">
+                {/* <Link href="/dashboard">Dashboard</Link> */}
+                <UserButton afterSignOutUrl="/" />
+              </div>
+            ) : (
+              <div className="sm:flex sm:gap-4">
+                <Link
+                  href="/sign-in"
+                  className="block rounded-mdpx-5 py-2.5 text-sm font-medium text-white transition"
+                >
+                  Login
+                </Link>
 
-      <div className="flex items-center gap-4">
-        <div className="sm:flex sm:gap-4">
-          <a
-            className="block rounded-mdpx-5 py-2.5 text-sm font-medium text-white transition"
-            href="#"
-          >
-            Login
-          </a>
+                <Link
+                  href="/sign-up"
+                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium  transition  sm:block"
+                >
+                  Register
+                </Link>
+              </div>
+            )}
 
-          <a
-            className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium  transition  sm:block"
-            href="#"
-          >
-            Register
-          </a>
-        </div>
-
-        <button
+            {/* <button
           className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
         >
           <span className="sr-only">Toggle menu</span>
@@ -69,12 +99,12 @@ function Header() {
 
             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
-        </button>
+        </button> */}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</header>
-  )
+    </header>
+  );
 }
 
-export default Header
+export default Header;
